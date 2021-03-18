1c7ba59fa3f153dc5516cd3a23f4f8b5f995d35e
Hannah <40068760+hanadrizz@users.noreply.github.com>
2021-03-18 01:02:17 +0100

    Update thebarkeeper.py

--- thebarkeeper.py
+++ thebarkeeper.py
@@ -28,7 +28,7 @@ pass_word = os.getenv('password')
 reddit = apraw.Reddit(client_id = "", client_secret = "", user_agent=useragent, username = "", password="")
 memefolder = os.getenv("memefolder")
 
-bannedsubs = ["poo", "kropotkistan"]
+bannedsubs = ["poo", "kropotkistan", "femboy", "trans", "feemagers"]
 
 # USERS AND CHANNELS
 
@@ -36,12 +36,21 @@ bottesting = 821087946941792258
 general = 752157598232477789
 memechannel = 752158323574308934
 logs = 821333966173765643
+pinboard = 821796457711534120
 
 modrole = 752158397255647252
+ownerrole = 821756270826618910
+
+# EMOJI
+
+moggers = 821358739336593429
+
+# INTENTS
 
 Intents = discord.Intents.default()
 Intents.members = True
 Intents.messages = True
+Intents.reactions = True
 
 output = ""
 
@@ -55,16 +64,14 @@ processed = []
 @commands.has_role(modrole)
 async def avgredditlookup(ctx):
     results = sum(processed) / len(processed)
-    await ctx.send(f"The average time to look up reddit posts is **{results}ms**")
+    await ctx.send(f"The average time to look up reddit posts is **{results} seconds**")
     
 
 @bot.command(pass_context=True)
-@commands.has_role(modrole)
+@commands.has_role(ownerrole)
 async def sendmessage(ctx, channel: int, *, arg):
-    print(ctx)
-    print(channel)
     cha = bot.get_channel(channel)
-    print(arg)
+    print(f"User {ctx.message.author} sent {arg}")
     await cha.send(arg)
 
 @bot.command(pass_context=True)
@@ -96,6 +103,14 @@ async def pardon(ctx, id: int):
     await ctx.guild.unban(user)
     await ctx.send(f"User <@!{id}> has been pardoned.")
 
+@bot.command(pass_context=True)
+@commands.has_role(modrole)
+async def modvote(ctx):
+    await ctx.message.add_reaction("👍")
+    await ctx.message.add_reaction("👎")
+    await ctx.message.add_reaction("👊")
+
+    
 
 # USER COMMANDS    
 
@@ -105,6 +120,9 @@ async def license(ctx):
         notice = license.read()
         await ctx.send(notice)
 
+@bot.command()
+async def booru(ctx, ):
+    pass
 
 
 @bot.command()
@@ -124,8 +142,12 @@ async def wiki(ctx, *, page):
             await ctx.channel.send(i)
         await ctx.channel.send(f"<{site.url}>")
     except Exception as inst:
-        output = "Error raised."
-        await ctx.send(f"{output}\n{inst}")
+        site = wikipage.page(page)
+        summary = wikipage.summary(page)
+        summary_list = textwrap.wrap(summary, 2000, break_long_words=False)
+        for i in summary_list:
+            await ctx.channel.send(i)
+        await ctx.channel.send(f"<{site.url}>")
 
 
 
@@ -146,14 +168,14 @@ async def wiki(ctx, *, page):
 @bot.command()
 async def redditsearch(ctx, sub):
     try:
-        print(f"{ctx.message.author.name} searched for an image in {sub}")
+        print(f"{ctx.message.author.name} searched for an image in r/{sub}")
         start_time = time.time()
         if sub == "traa":
             sub = "traaaaaaannnnnnnnnns"
         listing = []
         subreddit = await reddit.subreddit(sub)
         if sub.lower() in bannedsubs:
-            await ctx.send("Banned subreddit.")
+            await ctx.send("Subreddit is blocked in the server filter.")
             return
         elif subreddit.over18 == True:
             await ctx.send("No NSFW subreddits.")
@@ -198,14 +220,16 @@ async def jamaal(ctx):
     await ctx.send("man\nhttps://cdn.discordapp.com/attachments/821123717223415809/821642340359733258/f4xz3iryogm61.jpg")
     print(f"{ctx.message.author.name} jamaal")
 
-@bot.event
-async def on_ready():
-    print(f'Bot has connected to Discord!')
+@bot.command()
+async def hug(ctx, arg : discord.Member=None):
+    await ctx.send(f"<@{ctx.message.author.id}> is hugging <@{arg.id}>! :people_hugging:")
     
 @bot.command()
 async def sex(ctx):
     await ctx.send("thats what i have with your mom lmao 🔥🔥🔥🔥")
     print(f"{ctx.message.author.name} sex")
+
+# EVENTS
 
 @bot.event
 async def on_member_join(member):
@@ -216,15 +240,53 @@ async def on_member_join(member):
     print(f"{member} joined.")
 
 @bot.event
+async def on_reaction_add(reaction, user):
+    if hasattr(reaction.emoji, "id"):
+        if reaction.emoji.id == moggers:
+            if reaction.count >= 3:
+                if reaction.message.pinned == False:
+                    try:
+                        await reaction.message.pin()
+                    except:
+                        await reaction.message.channel.send("Maximum number of pins reached (50)")
+
+                    ctx = reaction.message.channel
+                    message = reaction.message
+                    user = message.author
+                    avatar = user.avatar_url
+                    pin = discord.Embed(color=0x9e85cc)
+                    pin.set_author(name=message.author.name, icon_url=avatar)
+                    pin.add_field(name="Channel:",value=message.channel.name, inline=False)
+                    if message.content:
+                        pin.add_field(name="Contents:", value=message.content, inline=False)
+                    if not message.attachments:
+                        pass
+                    else:
+                        image = message.attachments[0]
+                        img = image.url
+                        print(img)
+                        pin.set_image(url=img)
+                    board = bot.get_channel(pinboard)
+                    await board.send(f"Message pinned from <#{ctx.id}>", embed=pin)
+                    
+                    
+
+
+@bot.event
+async def on_ready():
+    print(f'Bot has connected to Discord!')
+
+@bot.event
 async def on_message_delete(message):
     channel = bot.get_channel(logs)
-    deletedmessage = discord.Embed(title=f"Message deleted.")
+    deletedmessage = discord.Embed(title=f"Message deleted.", color=0x9e85cc)
     author = message.author.name
     content = message.content
     user = message.author
     avatar = user.avatar_url
     deletedmessage.set_author(name=message.author.name, icon_url=avatar)
     deletedmessage.add_field(name="Author:", value=author, inline=False)
+    deletedmessage.add_field(name="Channel:", value=channel.name, inline=False)
     deletedmessage.add_field(name="Contents:", value=content, inline=False)
     await channel.send(embed=deletedmessage)
     print(f"{message.author.name} deleted a message")
