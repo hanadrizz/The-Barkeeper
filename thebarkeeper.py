c937e979e76329ca998dfa14687ddd244fe6e613
Hannah <adrizzplaysmc@gmail.com>
2021-03-19 02:22:01 +0100

    Update thebarkeeper.py

--- thebarkeeper.py
+++ thebarkeeper.py
@@ -41,9 +41,6 @@ dotenv.load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 guild = os.getenv("DISCORD_GUILD")
 
-from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
-
 reddit = apraw.Reddit(client_id = clientid, client_secret = clientsecret, user_agent=useragent, username = username, password=password)
 memefolder = os.getenv("memefolder")
 
